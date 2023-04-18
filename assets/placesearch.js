444c9790f89edb89352d352708603786fb8099c8
Liban200 <125518353+Liban200@users.noreply.github.com>
2023-04-18 19:19:19 -0400

    Made a search bar

---
+++ assets/placesearch.js
@@ -0,0 +1,7 @@
+placeSearch({
+    key: '70fdM0ulzAz6u7ykcfFXFUgAkVIzirTB',
+    container: document.querySelector('#place-search-input'),
+    collection: ["country", "city", "state"],
+    useDeviceLocation: true
+});
+
