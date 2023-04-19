b752c72f39b5a8078f50e723f695a77ea0c42c66
Shaun Nelson <shaunptnelson@gmail.com>
2023-04-18 20:42:39 -0400

    Created a bing map with API

--- assets/placesearch.js
+++ assets/placesearch.js
@@ -1,7 +1,19 @@
 placeSearch({
-    key: '70fdM0ulzAz6u7ykcfFXFUgAkVIzirTB',
-    container: document.querySelector('#place-search-input'),
-    collection: ["country", "city", "state"],
-    useDeviceLocation: true
-});
-
+  key: "70fdM0ulzAz6u7ykcfFXFUgAkVIzirTB",
+  container: document.querySelector("#place-search-input"),
+})
+  .on("results", (e) => {
+    console.log(e);
+    for (var i = 0; e.results.length; i++) {
+      if (e.results[i].type == "city") {
+        var coords = e.results[i].latLng;
+        console.log(coords);
+        // L.mapquest.map("map", {
+        //   center: [coords.lat, coords.lng],
+        //   //   layers: L.mapquest.tileLayer("map"),
+        //   zoom: 12,
+        // });
+      }
+    }
+  })
+  .open();
