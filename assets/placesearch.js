44a72f8bbe0d7ab7340b565836f4d7ed000f3e2d
Shaun Nelson <shaunptnelson@gmail.com>
2023-04-19 21:52:16 -0400

    Added Searchbar and search functionality

--- assets/placesearch.js
+++ assets/placesearch.js
@@ -1,19 +1,69 @@
-placeSearch({
+var ps = placeSearch({
   key: "70fdM0ulzAz6u7ykcfFXFUgAkVIzirTB",
   container: document.querySelector("#place-search-input"),
-})
-  .on("results", (e) => {
-    console.log(e);
-    for (var i = 0; e.results.length; i++) {
-      if (e.results[i].type == "city") {
-        var coords = e.results[i].latLng;
-        console.log(coords);
-        // L.mapquest.map("map", {
-        //   center: [coords.lat, coords.lng],
-        //   //   layers: L.mapquest.tileLayer("map"),
-        //   zoom: 12,
-        // });
-      }
+  collection: ["poi", "airport", "address", "adminArea"],
+});
+
+let markers = [];
+
+ps.on("change", (e) => {
+  markers.forEach(function (marker, markerIndex) {
+    if (markerIndex === e.resultIndex) {
+      markers = [marker];
+      marker.setOpacity(1);
+      map.setView(e.result.latlng, 11);
+    } else {
+      removeMarker(marker);
     }
-  })
-  .open();
+  });
+});
+
+ps.on("results", (e) => {
+  markers.forEach(removeMarker);
+  markers = [];
+
+  if (e.results.length === 0) {
+    map.setView(new L.LatLng(0, 0), 2);
+    return;
+  }
+
+  e.results.forEach(addMarker);
+  findBestZoom();
+});
+
+ps.on("cursorchanged", (e) => {
+  markers.forEach(function (marker, markerIndex) {
+    if (markerIndex === e.resultIndex) {
+      marker.setOpacity(1);
+      marker.setZIndexOffset(1000);
+    } else {
+      marker.setZIndexOffset(0);
+      marker.setOpacity(0.5);
+    }
+  });
+});
+
+ps.on("clear", () => {
+  console.log("cleared");
+  map.setView(new L.LatLng(0, 0), 2);
+  markers.forEach(removeMarker);
+});
+
+ps.on("error", (e) => {
+  console.log(e);
+});
+
+function addMarker(result) {
+  let marker = L.marker(result.latlng, { opacity: 0.4 });
+  marker.addTo(map);
+  markers.push(marker);
+}
+
+function removeMarker(marker) {
+  map.removeLayer(marker);
+}
+
+function findBestZoom() {
+  let featureGroup = L.featureGroup(markers);
+  map.fitBounds(featureGroup.getBounds().pad(0.5), { animate: false });
+}
